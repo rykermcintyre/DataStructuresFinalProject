97bde0e1e859f6727e44a07be15b23edc85d68ed
Kieran DiGiorno <dougdigiorno@macbook-pro-2.dhcp.nd.edu>
2018-04-25 17:22:04 -0400

    RB Tree

--- project.h
+++ project.h
@@ -5,8 +5,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <utility>
 
 using namespace std;
+
+typedef enum {
+    DUMP_KEY,
+    DUMP_VALUE,
+    DUMP_KEY_VALUE,
+    DUMP_VALUE_KEY,
+} DumpFlag;
+typedef pair<string, string> Entry;
+extern const Entry NONE;
+
 
 // TrieNode struct
 
@@ -16,15 +28,46 @@ struct TrieNode {
 	vector<TrieNode*> children;
 };
 
-// Trie
+// RBTree struct
+struct RBTreeNode{
+    int data;
+    bool color;
+    RBTreeNode *left;
+    RBTreeNode *right;
+    RBTreeNode *parent;
+    RBTreeNode(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+    }
+};
 
 class Trie {
 public:
-	Trie();
-	~Trie();
-	void insert(const string word);
-	bool search(const string word);
-	bool deconstruct(TrieNode* node);
+    Trie();
+    ~Trie();
+    void insert(const string word);
+    bool search(const string word);
+    bool deconstruct(TrieNode *node);
 private:
-	TrieNode root;
+    TrieNode root;
+};
+
+class Map {
+public:
+    virtual void        insert(string &key, string &value) {}
+    virtual const Entry search(string &key) { return NONE; }
+    virtual void        dump(ostream &os, DumpFlag flag) {}
+};
+
+class RBTree : public Map {
+public:
+    RBTree();
+    ~RBTree();
+    void        insert(const string &key, const string &value);
+    const Entry search(string &key);
+    void        dump(ostream &os, DumpFlag flag);
+private:
+    map<string, string> entries;
 };
---
+++ rbtree.cpp
@@ -0,0 +1,36 @@
+// rbtree.cpp
+
+#include "project.h"
+
+RBTree::RBTree () {}
+
+RBTree::~RBTree () {}
+
+void RBTree::insert(const string &key, const string &value){
+    entries[key] = value;
+}
+
+const Entry RBTree::search(const string key){
+    auto result = entries.find(key);
+    if (result == entries.end()) return NONE;
+    else return *result;
+}
+
+void RBTree::dump(ostream &os, DumpFlag flag){
+    for (auto it = entries.begin; it != entries.end(); it++){
+        switch (flag){
+            case DUMP_KEY:
+                os << it->first << endl; 
+                break;
+            case DUMP_VALUE:
+                os << it->second << endl;
+                break;
+            case DUMP_KEY_VALUE:
+                os << it->first << "\t" << it->second << endl;
+                break;
+            case DUMP_VALUE_KEY:
+                os << it->second << "\t" << it->first << endl;
+                break;
+        }
+    }
+}
