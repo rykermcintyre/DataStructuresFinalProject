fa4e5785a86be1621536987a42c32d15da797cbf
Rayyan Karim <rkarim0598@gmail.com>
2018-04-29 12:00:35 -0400

    made hash unsigned int (no negatives)

--- hash.cpp
+++ hash.cpp
@@ -3,11 +3,11 @@
 int main() {
 	string password;
 	while (cin >> password) {
-		long long hash = 83;
+		unsigned long long hash = 83;
 		for (size_t i = 0; i < password.size(); i++) {
 			hash = (hash * 38921) ^ (93179 * (password[i] - 8));
 		}
-		if (hash < 0) hash = -hash;
+		//if (hash < 0) hash = -hash;
 		cout << hash << endl;
 	}
 	return 0;
