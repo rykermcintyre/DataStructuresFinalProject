04ca354b0e9ffeeceac241c1c58512f7e4efb23b
Ryker McIntyre <rykermcintyre@rykers-mbp.dhcp.nd.edu>
2018-05-05 17:15:22 -0400

    buttons buttons buttons

--- bob.py
+++ bob.py
@@ -9,30 +9,42 @@ class Window(QWidget):
     
         QWidget.__init__(self, parent)
         
-        button = QPushButton(self.tr("Click me"))
+        button = QPushButton(self.tr("Login"))
+        create = QPushButton(self.tr("Create Account"))
         self.resultLabel = QLabel(self.tr("..."))
 
         # Create Textbox
         self.username = QLineEdit()
         self.password = QLineEdit()
+        self.usernameLabel = QLabel(self.tr("username: "))
+        self.passwordLabel = QLabel(self.tr("password: "))
         #textbox.move(50, 50)
         #textbox.resize(280, 40)
         # New style: uses the connect method of a pyqtSignal object.
         self.connect(button, SIGNAL("clicked()"), self.handleClick)
+        self.connect(create, SIGNAL("clicked()"), self.handleCreate)
         
         # Old style: uses the SIGNAL function to describe the signal.
         self.connect(self, SIGNAL("sendValue(PyQt_PyObject)"), self.handleValue)
         
-        layout = QVBoxLayout(self)
-        layout.addWidget(button)
-        layout.addWidget(self.resultLabel)
-        layout.addWidget(self.username)
-        layout.addWidget(self.password)
+        layout = QGridLayout(self)
+        layout.addWidget(button, 2, 0)
+        layout.addWidget(self.resultLabel, 2, 1)
+        layout.addWidget(self.username, 1, 0)
+        layout.addWidget(self.password, 1, 1)
+        layout.addWidget(self.usernameLabel, 0, 0)
+        layout.addWidget(self.passwordLabel, 0, 1)
     def handleClick(self):
         # Old style: emits the signal using the SIGNAL function.
         user = self.username.text()
         passwd = self.password.text()
-        self.emit(SIGNAL("sendValue(PyQt_PyObject)"), {user: passwd})
+        self.emit(SIGNAL("sendValue(PyQt_PyObject)"), [user, passwd])
+    
+    def handleCreate(self):
+        # Old style: emits the signal using the SIGNAL function.
+        user = self.username.text()
+        passwd = self.password.text()
+        self.emit(SIGNAL("sendValue(PyQt_PyObject)"), [user, passwd])
         
     def handleValue(self, value):
         self.resultLabel.setText("Submitting request...")
@@ -43,5 +55,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
     window.show()
-    window.resize(600, 600)
+    window.resize(600, 10)
     sys.exit(app.exec_())
